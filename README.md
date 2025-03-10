9c473ad60098891658c20fb5a3d35bfacc667f57
CrystalW11 <crystal.warmack@gmail.com>
2025-03-10 15:10:25 -0600

    updated contact.jsx

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ Services page
 ![alt text](image-2.png)
 
 Contact 
-![alt text](image-3.png)
+![alt text](image-6.png)
 
 super basic rough draft. 
--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -3,15 +3,39 @@
 // src/views/Contact.jsx
 import React from "react";
 import "../static/index.css";
+import locationImage from "../assets/location.png";// import the image here
+import phoneImage from "../assets/phone.png";
+import emailImage from "../assets/email.png";
 
 const Contact = () => {
   return (
     <div className="container-fluid contact-container">
       <div className="contact-info">
         <h1>Contact Us</h1>
-        <p>Email: example@example.com</p>
-        <p>Phone: 123-456-7890</p>
-        <p>Address: 123 Main St, City, Country</p>
+        <p>
+          <img
+            src={emailImage}
+            alt="Phone Icon"
+            style={{ width: "20px", height: "20px", marginLeft: "10px" }} // You can adjust the style here
+          />
+          Email: example@example.com
+        </p>
+        <p>
+          <img
+            src={phoneImage}
+            alt="Phone Icon"
+            style={{ width: "20px", height: "20px", marginLeft: "10px" }} // You can adjust the style here
+          />
+          Phone: 123-456-7890
+        </p>
+        <p>
+          <img
+            src={locationImage}
+            alt="Location Icon"
+            style={{ width: "20px", height: "20px", marginLeft: "10px" }} // You can adjust the style here
+          />
+          Address: 123 Main St, City, Country
+        </p>
       </div>
     </div>
   );
