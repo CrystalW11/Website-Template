7eea861abba72b45e4d26700d8ca2e8dc5932a84
CrystalW11 <crystal.warmack@gmail.com>
2025-04-13 13:43:28 -0600

    updated Home page and logo picture

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,27 +1,32 @@
 /** @format */
 
 import React from "react";
-import background from "../assets/background.jpg"; // Corrected import path
+import lower_deck from "../assets/lower_deck.png"; // Corrected import path
 
 const Home = () => {
   return (
-    <div
+    <div>
+    {/* <div
       style={{
-        backgroundImage: `url(${background})`, // Use the imported image
+        backgroundImage: `url(${lower_deck})`, // Use the imported image
         backgroundSize: "cover",
         backgroundPosition: "center",
-        height: "100vh",
+        height: "vh",
+        width:"80vh",
         margin: 0,
         paddingLeft: 10,
         paddingRight: 10,
         border: "2px solid white", // Adds a white border with a width of 2px
       }}>
-      <div className="container">
+            <div className="container">
       <h1 className="white-text">Welcome to <span style={{color:'red'}}>Red</span> Tail river Ranch!</h1>
       <p className="white-text">
         This is the home page of your winery application.
         </p>
         </div>
+      </div> */}
+      <img src={lower_deck} alt="lower_deck" style={{ width: "50%", height: "50vh" }} />
+
     </div>
   );
 };
