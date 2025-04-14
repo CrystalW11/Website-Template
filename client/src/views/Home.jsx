c672be57d526220b67216e6b19d057226051f1cf
CrystalW11 <crystal.warmack@gmail.com>
2025-04-14 14:54:22 -0600

    updated codes

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -16,22 +16,3 @@ const Home = () => {
 export default Home;
 
 
-{/* <div
-      style={{
-        backgroundImage: `url(${lower_deck})`, // Use the imported image
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "vh",
-        width:"80vh",
-        margin: 0,
-        paddingLeft: 10,
-        paddingRight: 10,
-        border: "2px solid white", // Adds a white border with a width of 2px
-      }}>
-            <div className="container">
-      <h1 className="white-text">Welcome to <span style={{color:'red'}}>Red</span> Tail river Ranch!</h1>
-      <p className="white-text">
-        This is the home page of your winery application.
-        </p>
-        </div>
-      </div> */}
